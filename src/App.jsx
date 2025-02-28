26deef756b09799dd15970de527ee8c8f9de278b
Jiří Strnadel <Juzba88@gmail.com>
2025-02-28 21:18:52 +0100

    done - upload data on firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,93 @@
-import './App.scss';
-import { useState, useEffect } from 'react';
-import { collection, getDocs, deleteDoc,doc } from 'firebase/firestore';
-import { projectFirestore } from './firebase/config';
+import "./App.scss";
+import { useState, useEffect } from "react";
+import { collection, deleteDoc, doc, onSnapshot, setDoc, addDoc } from "firebase/firestore";
+import { projectFirestore } from "./firebase/config";
 
 const App = () => {
-    const [error, setError] = useState(false);
-    const [data, setData] = useState(false);
+	// usestate pro formulář
+	const [title, setTitle] = useState("");
+	const [minAge, setMinAge] = useState("");
+	const [time, setTime] = useState("");
 
-    const deleteMovie = async(id) => {
-        await deleteDoc(doc(projectFirestore, "movies", id))
-    };
+	const [error, setError] = useState(false);
+	const [data, setData] = useState([]);
 
-    useEffect(() => {
-        const usersCollection = collection(projectFirestore, 'movies');
-        getDocs(usersCollection)
-            .then((snapshot) => {
-                if (snapshot.empty) {
-                    setError('Žádné data');
-                } else {
-                    let result = [];
-                    snapshot.docs.forEach((oneDoc) => result.push({ id: oneDoc.id, ...oneDoc.data() }));
-                    setData(result);
-                }
-            })
-            .catch((err) => setError(err.message));
-    }, []);
+	const onSubmit = async (e) => {
+		e.preventDefault();
 
-    return (
-        <div className="app">
-            <h1>Databáze Firebase</h1>
-            {data ? (
-                data.map(({ id, title, minage, time }) => {
-                    return (
-                        <section key={id}>
-                            <p>{`${title},  ${minage}+,  ${time}min`}</p>
-                            <button onClick={() => deleteMovie(id)}>Smazat</button>
-                        </section>
-                    );
-                })
-            ) : (
-                <p>Loading....</p>
-            )}
-            <p>{error}</p>
-        </div>
-    );
+		if (title && minAge && time) {
+			console.log("splneno");
+			const newMovie = { title: title, minage: minAge, time: time };
+
+			try {
+				// Přidá object nebo přepíše object se stejným id
+				// await setDoc(doc(projectFirestore, "movies", "id-zde"), newMovie)
+
+				// Přidá object s automaticky přiřazeným id
+				await addDoc(collection(projectFirestore, "movies"), newMovie)  
+	
+				setTitle("")
+				setMinAge("")
+				setTime("")
+				
+			} catch (error) {
+				setError("Fim nebyl přidán " + error.message)
+			}
+
+		} else {
+			console.log("nesplneno");
+		}
+	};
+
+	const deleteMovie = async (id) => {
+		await deleteDoc(doc(projectFirestore, "movies", id));
+	};
+
+	useEffect(() => {
+		const unsubscribe = onSnapshot(
+			collection(projectFirestore, "movies"),
+			(snapshot) => {
+				if (snapshot.empty) {
+					setError("Žádné data");
+					setData([]);
+				} else {
+					let result = [];
+					snapshot.docs.forEach((oneDoc) => result.push({ id: oneDoc.id, ...oneDoc.data() }));
+					setData(result);
+					setError(false);
+				}
+			},
+			(err) => setError(err.message)
+		);
+
+		return () => unsubscribe();
+	}, []);
+
+	return (
+		<div className="app">
+			<h1>Databáze Firebase</h1>
+
+			<form onSubmit={onSubmit}>
+				<input type="text" placeholder="Název filmu" onChange={(e) => setTitle(e.target.value)} value={title} />
+				<input type="number" min={0} placeholder="Min věk" onChange={(e) => setMinAge(e.target.value)} value={minAge} />
+				<input type="number" min={0} placeholder="Doba trvání" onChange={(e) => setTime(e.target.value)} value={time} />
+				<input type="submit" value={"Odeslat"} />
+			</form>
+
+			{data.length > 0 ? (
+				data.map(({ id, title, minage, time }) => {
+					return (
+						<section key={id}>
+							<p>{`${title},  ${minage}+,  ${time}min`}</p>
+							<button onClick={() => deleteMovie(id)}>Smazat</button>
+						</section>
+					);
+				})
+			) : (
+				<p>{error || "Loading...."}</p>
+			)}
+		</div>
+	);
 };
 
 export default App;
